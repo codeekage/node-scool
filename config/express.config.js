b6cc0c20c04ad6d175d2c36df02756402066fa5f
codekage <agiriabrahamjunior@gmail.com>
2017-11-04 18:10:25 +0100

    uploading-img

---
+++ config/express.config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const logger = require('morgan'),
+    busboyBodyParser = require('busboy-body-parser');
+
+module.exports = (app) => {
+    app.use(logger('dev'));
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
+    app.use(busboyBodyParser({ limit: '10mb' }));
+
+    //[*]Routes Configuration
+    let main = require('../routings/routing.js');
+    app.use('/api', main);
+};
---
+++ config/mongoose.config.js
@@ -0,0 +1,19 @@
+"use strict";
+let mongoose = require('mongoose');
+
+// activating Promises for mongoose
+mongoose.Promise = global.Promise;
+
+module.exports = (config) => {
+    var dbURI = config.dev.db;
+
+    // using new syntax for mongoose library
+    // check the mongoose doc for more info
+    mongoose.connect(dbURI, {
+        useMongoClient: true,
+    }).then(() => {
+        console.log(`Connected to ${dbURI}`);
+    }).catch((e) => {
+        throw e;
+    });
+};
